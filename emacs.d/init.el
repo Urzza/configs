c3d70f4618471e39996f66a914ed3860a32c39da
mc.asdfgf <mc.asdfgf@protonmail.com>
2018-05-16 03:23:13 +0700

    emacs init file

---
+++ emacs.d/init.el
@@ -0,0 +1,123 @@
+;; Packages management
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) ; Org-mode's repository
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+
+;; Theme
+(require 'ample-theme)
+
+;; Dired
+(require 'dired)
+(setq dired-recursive-deletes 'top)
+
+;; Imenu
+(require 'imenu)
+(setq imenu-auto-rescan      t)
+(setq imenu-use-popup-menu nil)
+(global-set-key (kbd "<f6>") 'imenu)
+
+;; Inhibit startup/splash screen
+(setq inhibit-splash-screen   t)
+(setq ingibit-startup-message t)
+
+;; Show-paren-mode settings
+(show-paren-mode t)
+(setq show-paren-style 'expression)
+
+;; Electric-modes settings
+(electric-pair-mode 1)
+(electric-indent-mode -1)
+
+;; Delete selection
+(delete-selection-mode t)
+
+;; Disable GUI components
+(tooltip-mode      -1)
+(menu-bar-mode     -1)
+(tool-bar-mode     -1)
+(scroll-bar-mode   -1)
+(blink-cursor-mode -1)
+(setq use-dialog-box  nil)
+(setq redisplay-dont-pause t)
+(setq ring-bell-function 'ignore)
+
+;; Disable backup/autosave files
+(setq make-backup-files        nil)
+(setq auto-save-default        nil)
+(setq auto-save-list-file-name nil)
+
+
+;; Linum plugin
+(require 'linum)
+(line-number-mode   t)
+(global-linum-mode  t)
+(column-number-mode t)
+(setq linum-format " %d")
+
+;; Fringe settings
+(fringe-mode '(8 . 0))
+(setq-default indicate-empty-lines t)
+(setq-default indicate-buffer-boundaries 'left)
+
+;; Display file size/time in mode-line
+(setq display-time-24hr-format t)
+(display-time-mode             t)
+(size-indication-mode          t)
+
+;; Line wrapping
+(setq word-wrap          t)
+(global-visual-line-mode t)
+
+;; IDO plugin
+(require 'ido)
+(ido-mode                      t)
+(icomplete-mode                t)
+(ido-everywhere                t)
+(setq ido-vitrual-buffers      t)
+(setq ido-enable-flex-matching t)
+
+;; Buffer Selection and ibuffer settings
+(require 'bs)
+(require 'ibuffer)
+(defalias 'list-buffers 'ibuffer)
+(global-set-key (kbd "<f2>") 'bs-show)
+
+;; Syntax highlighting
+(require 'font-lock)
+(global-font-lock-mode             t)
+(setq font-lock-maximum-decoration t)
+
+;; Indent settings
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width          4)
+(setq-default c-basic-offset     4)
+(setq-default standart-indent    4)
+(setq-default lisp-body-indent   4)
+(global-set-key (kbd "RET") 'newline-and-indent)
+(setq lisp-indent-function  'common-lisp-indent-function)
+
+;; Scrolling settings
+(setq scroll-step               1)
+(setq scroll-margin            10)
+(setq scroll-conservatively 10000)
+
+;; Short messages
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Clipboard settings
+(setq x-select-enable-clipboard t)
+
+;; End of file newlines
+(setq require-final-newline    t)
+(setq next-line-add-newlines nil) 
+
+;; Highlight search resaults
+(setq search-highlight        t)
+(setq query-replace-highlight t)
+
+;; Set your lisp system and, optionally, some contribs
+(load (expand-file-name "~/quicklisp/slime-helper.el"))
+(setq inferior-lisp-program "/usr/bin/sbcl")
+(setq slime-contribs '(slime-fancy))
